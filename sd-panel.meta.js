143d968cee65fb5908d50e0c3ba8fd3d0a38211a
amsdpanel-lab <amsdpanel@gmail.com>
2026-05-04 05:50:06 +0200

    sd-panel.meta.js

---
+++ sd-panel.meta.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         SD Panel
+// @namespace    amimirsh-watch-sorter
+// @version      1.5.0
+// @description  Foresight Watch Sorter by Amir Hossein Mirshekari @amimirsh
+// @match        https://sort-eu.aka.amazon.com/foresight*
+// @match        https://trans-logistics-eu.amazon.com/sortcenter/tantei*
+// @match        https://stem-eu.corp.amazon.com/node/*/equipment*
+// @grant        GM_xmlhttpRequest
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_addValueChangeListener
+// @grant        GM_openInTab
+// @grant        unsafeWindow
+// @noframes
+// @connect      trans-logistics-eu.amazon.com
+// @connect      trans-logistics.amazon.com
+// @connect      vinyaas-ui-eu.amazon.com
+// @connect      skynet.amazon.dev
+// @connect      stem-eu.corp.amazon.com
+// @run-at       document-start
+// @updateURL    https://amsdpanel-lab.github.io/sd-panel/sd-panel.meta.js
+// @downloadURL  https://amsdpanel-lab.github.io/sd-panel/sd-panel.user.js
+// ==/UserScript==
